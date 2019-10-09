4cdf00dc83a725adccedb51b9d48af036313045e
Neil Peterson <neilpeterson@outlook.com>
2019-10-09 08:24:28 -0700

    TS

--- deployment-ops-full/deploy-inprogress.sh
+++ deployment-ops-full/deploy-inprogress.sh
@@ -110,11 +110,9 @@ az storage blob upload-batch --destination $BLOB_ENDPOINT --destination profiles
 
 # Create oncall table
 AZURE_STORAGE_ACCT=$(az storage account keys list -n $AZURE_STORAGE_ACCT -g $azureResourceGroup --query [0].value -o tsv)
-
-sleep 60m
-
-apt-get install python-pip -y
-pip install azure
+apt-get update -y
+apt-get install python-pip3 -y
+pip3 install azure.cosmosdb.table
 python3 tailwind-reference-deployment-sandbox/deployment-ops-full/azure-table.py
 
 # Notes
